44f5a03ac9c09caccdc9148dd2c5d8fbea5c88cc
FlooferLand <flooferland@users.noreply.github.com>
2024-11-22 03:44:16 +0200

    Slightly improve  performance

--- grid/pixel/SubPixel.gd
+++ grid/pixel/SubPixel.gd
@@ -11,13 +11,14 @@ signal updated(pixel: Pixel, subpixel: SubPixel)
 var hovering := false
 
 func _enter_tree() -> void:
-	color = pixel.color_off
+	update_colour()
+
 	# Getting the colour
-	var update_colours := func():
+	var init_colour_preference := func():
 		var col: Color = Autoload.settings.subpixel_colours[id]
 		color_on = col if col != null else color
-	Autoload.settings.changed.connect(update_colours)
-	update_colours.call()
+	Autoload.settings.changed.connect(init_colour_preference)
+	init_colour_preference.call()
 
 func _ready() -> void:
 	# Connecting signals
@@ -25,13 +26,15 @@ func _ready() -> void:
 		func():
 			pixel.canvas.subpixel_info.hovering = self
 			hovering = true
+			update_colour()
 	)
 	mouse_exited.connect(
 		func():
 			hovering = false
+			update_colour()
 	)
 
-func _process(_delta: float) -> void:
+func update_colour() -> void:
 	color = pixel.color_off.lerp(color_on, _on)
 	if hovering:
 		color = color.lightened(0.1)
@@ -40,4 +43,5 @@ func get_subpixel() -> float:
 	return _on
 func set_subpixel(enabled: float) -> void:
 	_on = enabled
+	update_colour()
 	emit_signal("updated", pixel, self)
