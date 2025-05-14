f4d286859cd1c00b033dc6cf3e4405d65d0804c8
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-05-15 02:11:19 +0300

    Improved itch.io export script

--- addons/editor/export/export.gd
+++ addons/editor/export/export.gd
@@ -1,6 +1,18 @@
 @tool
+class_name EditorExportScript
 extends Window
 
 func _ready() -> void:
 	visible = false
 	close_requested.connect(queue_free)
+
+# Gets the build name for a channel
+func get_build_name(channel: String) -> String:
+	match channel:
+		"windows": return "Subpix.Windows.zip"
+		"linux": return "Subpix.Linux.zip"
+		"macos": return "Subpix.MacOS.zip"
+		"web": return "Subpix.Web.zip"
+		_:
+			push_error("Unknown platform with channel %s" % channel)
+			return ""
--- addons/editor/export/push_builds.gd
+++ addons/editor/export/push_builds.gd
@@ -1,6 +1,7 @@
 @tool
 extends Node
 
+@export var export: EditorExportScript
 @export var progress_bar: ProgressBar
 @export var start_button: Button
 @export var output: VBoxContainer
@@ -24,19 +25,29 @@ func _enter_tree() -> void:
 		thread.wait_to_finish()
 
 func push_build(channel: String) -> void:
-	starting_label.call_deferred("set_text", "Building %s.." % channel)
+	var zip_name := export.get_build_name(channel)
+	var out_file_name := ""
+	if channel == "web":
+		starting_label.call_deferred("set_text", "Unzipping %s.." % channel)
+		_extract_web_files(zip_name)
+		progress_bar.call_deferred("set_value", progress_bar.get_value() + 1)
+		out_file_name = "web/"
+	else:
+		out_file_name = zip_name
+	
+	# Building
 	var version: String = ProjectSettings.get_setting("application/config/version")
-	var cli_output: PackedStringArray = ["Pushing out channels/%s" % channel]
+	var cli_output: Array[String] = ["Pushing out channels/%s" % channel]
 	var exit_code := OS.execute("butler", [
-		"push", "builds/"+channel, "flooferland/subpix:"+channel,
+		"push", "builds/"+out_file_name, "flooferland/subpix:"+channel,
 		"--fix-permissions", "--userversion", version
 	], cli_output, true)
 	#var exit_code := OS.execute("butler", [], cli_output, true)
 	cli_output.append("Exit code %s" % exit_code)
-	push_build_output(channel, cli_output)
+	push_build_output(channel, cli_output.duplicate())
 	progress_bar.call_deferred("set_value", progress_bar.get_value() + 1)
 
-func push_build_output(channel: String, info: PackedStringArray) -> void:
+func push_build_output(channel: String, info: Array[String]) -> void:
 	var panel := PanelContainer.new()
 	panel.add_theme_stylebox_override("panel", stylebox_bg)
 	
@@ -60,12 +71,31 @@ func push_build_output(channel: String, info: PackedStringArray) -> void:
 	panel.add_child(container)
 	output.call_deferred("add_child", panel)
 
+func _extract_web_files(zip_name: String):
+	# Removing past build directory
+	DirAccess.remove_absolute("res://builds/web/")
+	
+	# Extracting
+	var reader := ZIPReader.new()
+	reader.open("res://builds/"+zip_name)
+	DirAccess.make_dir_absolute("res://builds/web/")
+	var root_dir := DirAccess.open("res://builds/web/")
+	var files := reader.get_files()
+	for file_path in files:
+		if file_path.ends_with("/"):  # If the current entry is a directory.
+			root_dir.make_dir_recursive(file_path)
+			continue
+		root_dir.make_dir_recursive(root_dir.get_current_dir().path_join(file_path).get_base_dir())
+		var file := FileAccess.open(root_dir.get_current_dir().path_join(file_path), FileAccess.WRITE)
+		var buffer := reader.read_file(file_path)
+		file.store_buffer(buffer)
+
 func _on_push_builds_pressed() -> void:
 	for child in output.get_children():
 		child.queue_free()
 	starting_label.text = "Building.."
 	progress_bar.value = 0
-	progress_bar.max_value = len(channels)
+	progress_bar.max_value = len(channels) + 1  # +1 is the web unzip
 	for channel in channels:
 		var thread := Thread.new()
 		thread.start(push_build.bind(channel))
